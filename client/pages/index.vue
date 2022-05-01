94d76eb2770293c4352181102f287327199060c3
Sheyin Nkom <sheyin.nkom@gmail.com>
2022-05-01 06:43:22 +0100

    Scaffold nuxt client app

---
+++ client/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Index'
+})
+</script>
